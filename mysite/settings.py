c1aa02db61b901231381d1865bd169eba49ea7ce
leanvega1 <leandrogvega@gmail.com>
2019-06-03 12:29:52 -0300

    habilitando css

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-INTERNAL_IPS = '127.0.0.1'
-
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, '/static/'),
 )
+
+INTERNAL_IPS = '127.0.0.1'
+
