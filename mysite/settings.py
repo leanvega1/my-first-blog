d7e0b99bbaa4315f882bacf4d00b94b5ccf5f92d
leanvega1 <leandrogvega@gmail.com>
2019-06-05 16:01:57 -0300

    intento1

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o0s@n32g%+-=4ew%rk%**7x43ruyvd87wj6)8c2y9^6oisuqse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -84,15 +85,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-	    'USER': 'name',
-	    'PASSWORD': '',
-	    'HOST': 'localhost',
-	    'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -142,3 +138,6 @@ LOGIN_REDIRECT_URL = '/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+django_heroku.settings(locals())
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for mysite project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from django.core.wsgi import get_wsgi_application
 
-application = DjangoWhiteNoise(application)
-
+application = get_wsgi_application()
